2a9f88eb00c0de5f6a8723029c7f9d6d214237b4
niko.jartti <niko.jartti@STUDY22P-142514>
2023-04-17 12:37:26 +0300

    moi taas

---
+++ harjoitus 2 opiskelija/harjoitus 2 opiskelija/Program.cs
@@ -0,0 +1,20 @@
+﻿using harjoitus_2_opiskelija;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        opiskelija OpiskelijaOlio = new opiskelija("Pekka", "21", 4);
+
+
+        OpiskelijaOlio.Nimi = "Jonne";
+        OpiskelijaOlio.OpiskelijaID = "100";
+        OpiskelijaOlio.Opintopisteet = 4;
+
+        Console.WriteLine(OpiskelijaOlio.TulostaData());
+
+        OpiskelijaOlio.MuokkaaOpintopisteita();
+
+        Console.WriteLine(OpiskelijaOlio.TulostaData());
+    }
+}
---
+++ harjoitus 2 opiskelija/harjoitus 2 opiskelija/opiskelija.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_2_opiskelija
+{
+    internal class opiskelija
+    {
+        public string Nimi { get; set; }
+        public string OpiskelijaID { get; set; }
+        public int Opintopisteet { get; set; }
+
+        public opiskelija(string _Nimi, string _OpiskelijaID, int _Opintopisteet)
+        {
+            Nimi = _Nimi;
+            OpiskelijaID = _OpiskelijaID;
+            Opintopisteet = _Opintopisteet;           
+        }
+
+        public string TulostaData()
+        {
+            string OpiskelijaTiedot = "Nimi " + Nimi + ". OpiskelijaID " + OpiskelijaID + ". Opintopisteet " + Opintopisteet.ToString();
+            return OpiskelijaTiedot;
+        }
+
+        public int MuokkaaOpintopisteita()
+        {
+            Opintopisteet = 10;
+            Console.WriteLine("Opintopisteiden määrä muokattu: " + Opintopisteet);
+            return Opintopisteet;
+        }
+    }
+}
