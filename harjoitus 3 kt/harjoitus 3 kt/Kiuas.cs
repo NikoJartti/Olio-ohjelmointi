ddd8f2c01ad7cf1ff75d9099083dfc4745a63fcc
niko.jartti <niko.jartti@STUDY22P-142514>
2023-04-17 14:00:50 +0300

    harjoitus kolomone

---
+++ harjoitus 3 kt/harjoitus 3 kt/Kiuas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_3_kt
+{
+    internal class Kiuas
+    {
+        public string Valmistaja { get; set; }
+        public int lämpötila { get; set; }
+        public int kosteus { get; set; }
+        public string päällä { get; set; }
+
+        public Kiuas(string _Valmistaja, int _lämpötila, int _kosteus, string _päällä)
+        {
+            Valmistaja = _Valmistaja;
+            lämpötila = _lämpötila;
+            kosteus = _kosteus;
+            päällä = _päällä;
+
+
+        }
+
+        public string TulostaData()
+        {
+            string KiuasTiedot = "Valmistaja " + Valmistaja + ". Lämpötila " + lämpötila + "°C. Kosteus " + kosteus.ToString() +"%. Päällä " + päällä.ToString();
+            return KiuasTiedot;
+        }
+
+        public int MuokkaaLämpötilaa()
+        {
+            lämpötila = 110;
+            return lämpötila;
+        }
+        public string Virta()
+        {
+            päällä = "kyllä";
+            return päällä;
+        }
+    }
+}
---
+++ harjoitus 3 kt/harjoitus 3 kt/Program.cs
@@ -0,0 +1,26 @@
+﻿using harjoitus_3_kt;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Kiuas kiuasOlio = new Kiuas("harvia", 100, 90, "ei");
+
+
+        kiuasOlio.Valmistaja = "harvia";
+        kiuasOlio.lämpötila = 10;
+        kiuasOlio.kosteus = 90;
+        kiuasOlio.päällä = "ei";
+
+        Console.WriteLine(kiuasOlio.TulostaData());
+
+        Console.WriteLine("Paina ENTER, jos haluat kiukaan päälle");
+        Console.ReadLine();
+
+        kiuasOlio.Virta();
+
+        kiuasOlio.MuokkaaLämpötilaa();
+
+        Console.WriteLine(kiuasOlio.TulostaData());
+    }
+}
