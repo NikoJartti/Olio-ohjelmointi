29f36f75c7c78e6145705488da36e3d2bc80e8a3
niko.jartti <niko.jartti@STUDY22P-142514>
2023-04-17 10:15:15 +0300

    helo

---
+++ Auto/Auto/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos
+{
+    internal class Auto
+    {
+        public string Nimi { get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
+
+
+
+
+
+        public string TulostaData()
+        {
+            string AutonTiedot = "Nimi: " + Nimi + ", Nopeus: " + Nopeus.ToString() + " km/h, Renkaat: " + Renkaat.ToString();
+
+            return AutonTiedot;
+        }
+    }
+}
---
+++ Auto/Auto/Program.cs
@@ -0,0 +1,14 @@
+﻿using Autos;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Auto AutoOlio = new Auto();
+
+        AutoOlio.Nimi = "BMW";
+        AutoOlio.Nopeus = 125;
+        AutoOlio.Renkaat = 5;
+
+        Console.WriteLine(AutoOlio.TulostaData());
+    }
+}
