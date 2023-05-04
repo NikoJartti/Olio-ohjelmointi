04b2d029e60701b90d97f7d6fb2906e11a7cd395
Niko Jartti <niko.jartti@gmail.com>
2023-05-04 12:46:13 +0300

    harjoitus 12

---
+++ harjoitus 12/harjoitus 12/Program.cs
@@ -0,0 +1,23 @@
+﻿using harjoitus_12;
+
+class korttipakka
+{
+    static void Main()
+    {
+
+        Korttipakka pakka = new Korttipakka();
+
+        Console.WriteLine("Pakka ei ole sekoitettu");
+        pakka.TulostaKortit();
+
+
+
+        pakka.SekoitaKortit();
+
+        Console.WriteLine("\nKortit on sekoitettu:");
+
+        pakka.TulostaKortit();
+
+
+    }
+}
---
+++ harjoitus 12/harjoitus 12/sekoitakortit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_12
+{
+    class Korttipakka
+    {
+        private List<string> kortit;
+
+        public Korttipakka()
+        {
+            kortit = new List<string>();
+            LisaaKortit();
+        }
+
+        private void LisaaKortit()
+        {
+            string[] maat = { "Hertta", "Risti", "Ruutu", "Pata" };
+            string[] numerot = { "ässä", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13" };
+
+            foreach (string maa in maat)
+            {
+                foreach (string numero in numerot)
+                {
+                    kortit.Add(numero + " " + maa);
+                }
+            }
+        }
+
+        public void TulostaKortit()
+        {
+            foreach (string kortti in kortit)
+            {
+                Console.WriteLine(kortti);
+            }
+        }
+
+        public void SekoitaKortit()
+        {
+            Random rng = new Random();
+            int n = kortit.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                string kortti = kortit[k];
+                kortit[k] = kortit[n];
+                kortit[n] = kortti;
+            }
+        }
+    }
+}
